1b685cb661d4390f09de3c9faf9cd1530ed48905
a.artamonov <a.artamonov@gb.ru>
2022-11-16 21:05:20 +0300

    Fix bugs and warnings. All is commented.

--- Program.cs
+++ Program.cs
@@ -8,45 +8,45 @@
 //     return count;
 // }
 
-// Console.WriteLine("Input as many numbers as you want. To stay input 's'.");
-// string s = Console.ReadLine();
-// string sNumber = String.Empty;
+// Console.WriteLine("Input as many numbers as you want. To stay input nothing.");
+// var sNumber = Console.ReadLine();
 // int countAll = 0;
-// while(s != "s")
+// while(sNumber != string.Empty)
 // {
-//     countAll = CountGreaterZero(Convert.ToInt32(s), countAll);
-//     s = Console.ReadLine();
+//         countAll = CountGreaterZero(Convert.ToInt32(sNumber), countAll);
+//         sNumber = Console.ReadLine();
 // }
 // Console.WriteLine($"Count of the numbers as greater than 0 is {countAll}");
+
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-void FindXY (double b1, double k1, double b2, double k2)
-{
-    if (k1 != k2)
-    {
-        double x = (b1 - b2)/(k2 - k1);
-        Console.WriteLine($"[{x}; {k1 * x + b1}]");
-    }
-    else
-    {
-        Console.WriteLine("No");
-    }
-}
+// void FindXY (double b1, double k1, double b2, double k2)
+// {
+//     if (k1 != k2)
+//     {
+//         double x = (b1 - b2)/(k2 - k1);
+//         Console.WriteLine($"[{x}; {k1 * x + b1}]");
+//     }
+//     else
+//     {
+//         Console.WriteLine("No");
+//     }
+// }
 
-Console.WriteLine("I can help solve a system of equations:");
-Console.WriteLine("y = k1 * x + b1");
-Console.WriteLine("y = k2 * x + b2");
-Console.WriteLine();
-Console.Write("Input k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("I can help solve a system of equations:");
+// Console.WriteLine("y = k1 * x + b1");
+// Console.WriteLine("y = k2 * x + b2");
+// Console.WriteLine();
+// Console.Write("Input k1: ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input b1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input k2: ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input b2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
 
-FindXY(b1, k1, b2, k2);
+// FindXY(b1, k1, b2, k2);
